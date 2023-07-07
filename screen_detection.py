5cc77479f1082f09a12fd2965da5e9d12f277b73
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-07-07 22:13:40 +0200

    Run black formatting

--- screen_detection.py
+++ screen_detection.py
@@ -5,8 +5,9 @@ import numpy as np
 from PIL import Image
 
 
-model = torch.hub.load("yolov5", 'custom', path="train_results/traine_best/weights/best.pt", source='local')
-model.conf = 0.6 # Define model confidence
+model = torch.hub.load(
+    "yolov5", 'custom', path="train_results/traine_best/weights/best.pt", source='local')
+model.conf = 0.6  # Define model confidence
 sct = mss()
 
 
@@ -15,13 +16,12 @@ while 1:
     monitor = {'top': 0, 'left': 0, 'width': w, 'height': h}
     img = Image.frombytes('RGB', (w, h), sct.grab(monitor).rgb)
     screen = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-    
+
     # Get result of model
     result = model(screen, size=1280)
-    
+
     cv2.imshow('Screen', result.render()[0])
 
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
-
