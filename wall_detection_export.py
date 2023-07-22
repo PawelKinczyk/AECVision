200af360fe3621eb6835599c9a0b608714630e6c
PawelKinczyk <pawel.kinczyk@outlook.com>
2023-07-22 15:33:55 +0200

    Add csv exporter to program

--- wall_detection_export.py
+++ wall_detection_export.py
@@ -7,6 +7,8 @@ import io
 import torch
 import numpy as np
 import pandas as pd
+import shutil
+import os
 
 
 def to_1280_format(h, w):
@@ -14,15 +16,18 @@ def to_1280_format(h, w):
     return ((w-1)*1280, (h-1)*1280, w*1280, h*1280)
 
 
-def convert_pdf(path_pdf, path_convert_pdf):
-    pdf_list = list(path_pdf.glob("**/*.pdf"))
-    doc = fitz.open(pdf_list[0])
-    page = doc.load_page(0)
-    pixmap = page.get_pixmap(dpi=300)
-    # convert_pdf = path_convert_pdf / "{0}.jpg".format(pdf_list[0].name)
-    # pixmap.save(convert_pdf)
-    return pixmap.tobytes("jpg")
-
+class Convert_pdf():
+    def __init__(self, path_pdf, path_convert_pdf):
+        self.pdf_list = list(path_pdf.glob("**/*.pdf"))
+        self.doc = fitz.open(self.pdf_list[0])
+        self.page = self.doc.load_page(0)
+        self.pixmap = self.page.get_pixmap(dpi=300)
+        # convert_pdf = path_convert_pdf / "{0}.jpg".format(pdf_list[0].name)
+        # pixmap.save(convert_pdf)
+    def return_pixmap(self):
+        return self.pixmap.tobytes("jpg")
+    def return_image(self):
+        return Image.open(io.BytesIO(self.pixmap))
 
 def crop_image(path_pdf, path_convert_pdf, hight_pixels, width_pixels, bytes_image):
     jpg_list = list(path_pdf.glob("**/*.pdf"))
@@ -57,7 +62,8 @@ path_pdf = Path("wall_detection_export/upload_pdf")
 path_convert_pdf = Path("wall_detection_export/convert_pdf")
 path_export_txt = Path("wall_detection_export/export_txt")
 
-data = convert_pdf(path_pdf=path_pdf, path_convert_pdf=path_convert_pdf)
+converter = Convert_pdf(path_pdf=path_pdf, path_convert_pdf=path_convert_pdf)
+data = converter.return_pixmap()
 # Chop pdf to 1280x1280
 jpg_list = list(path_pdf.glob("**/*.pdf"))
 # img = Image.open(jpg_list[0])
@@ -105,9 +111,16 @@ for file in path_convert_pdf.glob("**/*.jpg"):
     df["ymax"] = df["ymax"] + float(file_name[1]) * 1280
     df["xmin"] = df["xmin"] + float(file_name[2]) * 1280
     df["xmax"] = df["xmax"] + float(file_name[2]) * 1280
-    print(df)
+    # print(df)
     df_predictions = pd.concat([df_predictions, df])
-
+    image_prediction = path_export_txt / "image"
+    print(image_prediction)
+    results.save(save_dir=image_prediction)
+    for filename in os.listdir(image_prediction):
+        source_file_path = os.path.join(image_prediction, filename)
+        destination_file_path = os.path.join(path_export_txt, filename)
+        shutil.move(source_file_path, destination_file_path)
+    shutil.rmtree(image_prediction)
 df_predictions.to_csv(path_export_txt / "prediction.csv")
 # Combine labels into one txt file with x/y coordination and thickness of wall
 
